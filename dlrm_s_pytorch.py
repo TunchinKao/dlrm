90c1044cca03bbf6ffc952eab650ca50326c2b9f
TunchinKao <tunchinkao@gmail.com>
2021-10-14 21:09:21 +0800

    feat : fix typo

--- dlrm_s_pytorch.py
+++ dlrm_s_pytorch.py
@@ -356,7 +356,7 @@ class DLRM_Net(nn.Module):
                         [0,1,2,3],[4,5,6,7],[8,9,10],[11,12,13], [14,15,16], [17,18,19],
                         [20,22,23,24],[21,25]
                 ]
-                self.n_loccal_emb = len(embedding_ranks[ext_dist.my_local_rank])
+                self.n_local_emb = len(embedding_ranks[ext_dist.my_local_rank])
                 self.n_emb_per_rank = [len(embedding_ranks[i]) for i in range(len(embedding_ranks))]
                 self.local_emb_indices = embedding_ranks[ext_dist.my_local_rank]
 
